a919793ee464b61cb9a138f4a53f16ba0439d8bd
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2023-01-30 15:11:28 +0100

    started developing real raycasting

--- script.ts
+++ script.ts
@@ -13,8 +13,12 @@ interface Position {
 interface CanvasBody {
     color: Color
     position: Position,
-    width: number,
-    height: number,
+    sides: Boundry[]
+}
+
+interface Boundry {
+    start: Position,
+    end: Position
 }
 
 function GenerateRandomNumber(min: number, max: number): number {
@@ -67,65 +71,24 @@ main();
 
 async function main() {
 
-    for (let _ = 0; _ < 3; _++) {
-
-        let color: Color = {
-            Red: GenerateRandomNumber(0, 255),
-            Green: GenerateRandomNumber(0, 255),
-            Blue: GenerateRandomNumber(0, 255),
-            Alpha: GenerateRandomNumber(0, 255),
-        }
-
-        let width: number = GenerateRandomNumber(10, 1000);
-        let height: number = GenerateRandomNumber(10, 700);
-
-        let position: Position = { x: GenerateRandomNumber(0, canvasWidth - width), y: GenerateRandomNumber(0, canvasHeight - height) };
-
-        AddBody(position, color, width, height);
-    }
-
     document.addEventListener('mousemove', onMouseUpdate, false);
     document.addEventListener('mouseenter', onMouseUpdate, false);
 
 }
 
 
-async function scan(precision: number, start_position: Position, segments: number) {
+async function scan(start_position: Position) {
 
-    for (let i = 0; i < canvasWidth / precision; i++) {
-        DrawLine(start_position, { x: i * precision, y: 0 }, red, segments);
-        //await sleep(sleep_time);
-    }
-
-
-    for (let i = 0; i < canvasHeight / precision; i++) {
-        DrawLine(start_position, { x: canvasWidth, y: i * precision }, white, segments);
-        //await sleep(sleep_time);
-    }
-
-    for (let i = 0; i < canvasWidth / precision; i++) {
-        DrawLine(start_position, { x: canvasWidth - (i * precision), y: canvasHeight }, blue, segments);
-        //await sleep(sleep_time);
-    }
-
-    for (let i = 0; i < canvasHeight / precision; i++) {
-        DrawLine(start_position, { x: 0, y: canvasHeight - (i * precision) }, green, segments);
-        //await sleep(sleep_time);
+    for (let i = 0; i < 361; i++) {
+        DrawLine(start_position, { x: start_position.x + Math.sin(i) * 100, y: start_position.y + Math.cos(i) * 100 }, white);
     }
 }
 
-function AddBody(position: Position, color: Color, width: number, height: number) {
-    bodies.push({
-        color: color,
-        position: position,
-        width: width,
-        height: height,
-    });
+function CastRay(start_position: Position, direction: Position) {
+
 }
 
-function BodyContainsPoint(body: CanvasBody, position: Position): boolean {
-    return (body.position.x <= position.x && body.position.y <= position.y && body.position.x + body.width >= position.x && body.position.y + body.height >= position.y);
-}
+
 
 function DrawDot(position: Position, color: Color, radius: number) {
     ctx.beginPath();
@@ -134,43 +97,19 @@ function DrawDot(position: Position, color: Color, radius: number) {
     ctx.fill();
 }
 
-function DrawLine(start: Position, end: Position, color: Color, segments: number = 1) {
-    if (segments > 1) {
-        let segment_size: Position = { x: (end.x - start.x) / segments, y: (end.y - start.y) / segments }
-        let stop_iteration = false;
-        let previous_position_start: Position = { x: start.x, y: start.y };
-        let index = 0;
-        while (index < segments && !stop_iteration) {
-            ctx.beginPath();
-            ctx.moveTo(previous_position_start.x, previous_position_start.y);
+function DrawLine(start: Position, end: Position, color: Color) {
+    ctx.beginPath();
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.strokeStyle = `rgb(${color.Red},${color.Green},${color.Blue},${color.Alpha})`;
+    ctx.stroke();
 
-            previous_position_start.x += segment_size.x;
-            previous_position_start.y += segment_size.y;
-
-            for (let i = 0; i < bodies.length; i++) {
-                if (BodyContainsPoint(bodies[i], previous_position_start)){
-                    stop_iteration= true;
-
-                    break;
-                }
-            }
-
-            if (!stop_iteration) {
-                ctx.lineTo(previous_position_start.x, previous_position_start.y);
-                ctx.strokeStyle = `rgb(${color.Red},${color.Green},${color.Blue},${color.Alpha})`;
-                ctx.stroke();
-                index++;
-            }
-        }
-
-
-    } else {
-        ctx.beginPath();
-        ctx.moveTo(start.x, start.y);
-        ctx.lineTo(end.x, end.y);
-        ctx.strokeStyle = `rgb(${color.Red},${color.Green},${color.Blue},${color.Alpha})`;
-        ctx.stroke();
-    }
+    // for (let i = 0; i < bodies.length; i++) {
+    //     if (BodyContainsPoint(bodies[i], previous_position_start) && previous_position_start.x > 0 && previous_position_start.x < canvasWidth && previous_position_start.y > 0 && previous_position_start.x < canvasHeight) {
+    //         stop_iteration = true;
+    //         break;
+    //     }
+    // }
 
 }
 
@@ -186,11 +125,11 @@ function Sleep(ms: number) {
 
 function onMouseUpdate(e: MouseEvent) {
     mouse_position = { x: e.pageX, y: e.pageY };
-    DrawScan(100, mouse_position);
+    DrawScan(mouse_position);
 
 }
 
-function DrawScan(segments: number = 1, mouse_position: Position) {
-    //ClearCanvas();
-    scan(10, mouse_position, segments);
+function DrawScan(mouse_position: Position) {
+    ClearCanvas();
+    scan(mouse_position);
 }
