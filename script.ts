2906afa55b5cef9074d3bb1db32505f961ea38a8
SiposLevente <sipi.levente@gmail.com>
2023-03-03 22:58:17 +0100

    removed logging

--- script.ts
+++ script.ts
@@ -270,7 +270,6 @@ class KeyPressController {
                 bodyCounter++;
             }
 
-            console.log(bodyCounter + " " + sideCounter);
             if (!invalidMovement) {
                 player.SetPosition(nextPosition);
             }
